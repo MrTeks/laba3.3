ef0f4937d0d3cfe10555124ea0f31c6c4e7ef964
Teks <Teks@TEKS>
2023-10-02 00:01:11 +0700

    Добавьте файлы проекта.

---
+++ laba3.3/laba3.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	double a = 2.4;
+	double b = 1.3;
+	double x, y;
+	cout << "Введите х:";
+	cin >> x;
+	if (x < -2) {
+		y = (a * pow(cos(x), 2) - b * sin(pow(x, 2)));
+	}
+	if (x >= -2 && x <= 5) {
+		y = (b * log(x) + pow(x, 3));
+	}
+	if (x > 5) {
+		y = (sqrt(pow(x, 2) + a * b));
+	}
+	cout << "Значение у:" << y << endl;
+}
